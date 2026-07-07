ba68c6195e0eaa704f6cdeb40203251cca86be4c
Hossam Omar <hossamomar2nd@gmail.com>
2026-07-07 13:11:16 +0300

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<int> prefixSum(nums.size()+1, 0);
+        for(int i =0; i < nums.size(); i++){
+            prefixSum[i+1] = prefixSum[i] + nums[i];
+        }
+        for(int i =0; i < nums.size(); i++){
+            int left = prefixSum[i];
+            int right = prefixSum[nums.size()] - prefixSum[i+1];
+            if(left == right) return i;
+        }
+        return -1;
+    }
+};
