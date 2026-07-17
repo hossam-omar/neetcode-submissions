856900efeb92dca7176760f4f1d56debc0c7116c
Hossam Omar <hossamomar2nd@gmail.com>
2026-07-17 22:09:14 +0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string encoded;
+
+        for (string& s : strs) {
+            encoded += to_string(s.size());
+            encoded += '#';
+            encoded += s;
+        }
+
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int i = 0;
+
+        while (i < s.size()) {
+            int j = i;
+
+            while (s[j] != '#') {
+                j++;
+            }
+
+            int length = stoi(s.substr(i, j - i));
+
+            string word = s.substr(j + 1, length);
+            decoded.push_back(word);
+
+            i = j + 1 + length;
+        }
+
+        return decoded;
+    }
+};
